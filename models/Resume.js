7b1a0ad7129a9555149bfd07f2d4faf1850c12b1
Sophia Enakpoya <sophierithous@gmail.com>
2021-03-01 16:41:35 +0100

    set up server and db for resume builder

---
+++ models/Resume.js
@@ -0,0 +1,113 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ResumeSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  summary: {
+    type: String,
+  },
+
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+
+      location: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+    portfolio: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Resume = mongoose.model("resume", ResumeSchema);
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+//Load environment variables
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
+
+const app = express();
+
+//Bodyparser
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
