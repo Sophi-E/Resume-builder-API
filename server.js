53a8650cbd1e8a65a8a0a10292e532384f13f637
Sophia Enakpoya <sophierithous@gmail.com>
2021-03-03 10:40:31 +0100

    cors

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 //Bodyparser
 app.use(express.json({ extended: false }));
